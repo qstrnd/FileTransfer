24c9b58bbad239064598856563a80f892b4d207f
Andy <qstrnd@pm.me>
2026-06-22 21:34:37 +0200

    Initial Commit

---
+++ FileTransferTests/FileTransferTests.swift
@@ -0,0 +1,19 @@
+//
+//  FileTransferTests.swift
+//  FileTransferTests
+//
+//  Created by Andy on 22.06.26.
+//
+
+import Testing
+@testable import FileTransfer
+
+struct FileTransferTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
